3f59e6e4e2d4d2ef1a5f10484af2af8f60c40edf
justEngineer <av-paramonov@mail.ru>
2017-09-11 17:20:19 +0300

    Adding regex use case

--- main.cpp
+++ main.cpp
@@ -17,6 +17,23 @@ int main(int argc, char *argv[])
 int main(int argc, char *argv[])
 {
     std::string temp;
+    map<int,int> m;   //здесь храним пары степень-коэффициент
+        regex r("([\+-]?)([\d]]*)\\*?(x?)\\^?([\d]*)");
+        sregex_iterator pos(polynomial.cbegin(),polynomial.cend(),r);  //polynomial - строка, содержащая полином
+        sregex_iterator end;
+        for(;pos!=end;pos++)
+        {
+            int power=1;
+            int koef=0;
+            bool sign=1;
+            if(pos->str(4)!="") power = stoi(pos->str(4));
+            if(pos->str(3)!="x") power=0;
+            if(pos->str(1)=="-") sign=0;
+            if(pos->str(2)!="") koef=sign?(stoi(pos->str(2))):(-stoi(pos->str(2)));
+            else if(pos->str(3)=="x") koef=sign?1:-1;
+            else koef=0;
+            m[power]+=koef;
+        }
     std::cin >> temp;
     std::cout << "Hello World!" + temp << std::endl;
     system("PAUSE"); // <---
