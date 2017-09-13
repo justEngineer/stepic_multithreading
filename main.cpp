4319a8d10858eae5dec697c55ba36518879f3bbd
justEngineer <av-paramonov@mail.ru>
2017-09-13 15:59:02 +0300

    Lesson 1 learned: regexp & iterators

--- main.cpp
+++ main.cpp
@@ -16,108 +16,62 @@ int main(int argc, char *argv[])
 #include <regex>
 #include <string>
 
-//using namespace std;
+void getAnswer(const int &first, const int &second, std::string &answer) {
+    if (first != 0) {
+        if (first == 1) {
+            answer += std::to_string(second) + "*x"; //Вывод ключей и значений
+        }
+        else {
+            answer += std::to_string(second) + "*x^" + std::to_string(first); //Вывод ключей и значений
+        }
+    }
+    else {
+        answer += std::to_string(second);
+    }
+}
 
 int main(int argc, char *argv[])
 {
-/*
-    std::map<std::string, std::vector<int>> map;
-    std::vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    map["one"] = v;
+    std::string answer = "";
 
-
-    std::vector<int> v2;
-    v2.push_back(11);
-    v2.push_back(12);
-    v2.push_back(13);
-    map["two"] = v2;
-
-    for(const auto &kvp: map)
-    {
-       std::cout << kvp.first << std::endl;
-       for(auto &v: kvp.second)
-          std::cout << v << std::endl;
-    }
-
-    int arr[] = {1,2,3,4,5};
-
-    for(int &e: arr) {
-       e *= e;
-       std::cout << e << std::endl;
-    }
-//    system("PAUSE"); // <---
-    return 0;
-*/
-    std::string polynomial = "-2*x^5+5";
-    std::map<int,int> m;   //здесь храним пары степень-коэффициент
-/*    while (std::cin >> polynomial) {
-        if (polynomial == "") {
+    for (std::string polynomial; std::getline(std::cin, polynomial); answer = "") {
+        if (polynomial.empty()) {
             break;
         }
-*/
-//        std::regex r("([\+-]?)([\d]*)\\*?(x?)\\^?([\d]*)");
+        std::map<int,int> m;   //здесь храним пары степень-коэффициент
 
-//      std::regex words_regex("([\+-]?)([[:d:]]*)[\\*?]x?\^?([[:d:]]*)"/*, std::regex_constants::basic*/);
-//        std::regex words_regex("([\+-]?)([\d]*)\\*?(x?)\\^?([\d]*)");
-        std::regex words_regex("([\+-]?)([\\d]*)[\\*]?(x?)[\^]?([\\d]*)");
+        std::regex words_regex("([\+-]?)"       // Знак
+                               "([\\d]*)[\\*]?" // Коэффициент
+                               "(x)[[\^]?"       // Наличие неизвестного
+                               "([\\d]*)]?");     // Степень
         std::cmatch result;
-//        std::sregex_iterator pos(polynomial.begin(), polynomial.end(), words_regex);  //polynomial - строка, содержащая полином
-//        std::sregex_iterator end;
+        if (std::regex_search(polynomial.c_str(), result, words_regex)) {
 
+            std::sregex_iterator pos(polynomial.cbegin(), polynomial.cend(), words_regex);  //polynomial - строка, содержащая полином
+            std::sregex_iterator end;
 
-//        for(auto pos = pos.begin(), pos != pos.end(); pos++)
-        if (std::regex_search(polynomial.c_str(), result, words_regex))
-        {
-            std::cout << "Gotcha!!!" << std::endl;
-            for(auto &e: result) {
-               std::cout << e << std::endl;
+            for(; pos != end; pos++)
+            {
+                int power = 0;
+                int koef = 0;
+                int sign = 0;
+
+                if(pos->str(1) == "-") { sign = 1; }
+                if((pos->str(4) != "0") && (pos->str(4) != "")) { power = stoi(pos->str(4)) - 1; }
+                if(pos->str(2)!="") { koef = pow((-1),sign) * stoi(pos->str(2)) * (power + 1); }
+                else  { koef = pow((-1),sign) * (power + 1); }
+                m[power] += koef;
             }
-            /*
-            int power=1;
-            int koef=0;
-            bool sign=0;
-            int s=0;
-
-            if (result[3]=="x") {
-                if(result[1]=="-") {
-                    s=1;
-                }
-                koef = ((-1)^s)*stoi(result[2]);
-                power = stoi(result[4]) - 1;
+            // вывод первого выражения, без знака "+" впереди
+            auto item = m.rbegin();
+            getAnswer(item->first, item->second, answer);
+            // вывод последующих слагаемых, со знаком
+            for(auto item = ++m.rbegin(); item != m.rend(); ++item) {
+                if (item->second > 0) { answer += "+"; }
+                getAnswer(item->first, item->second, answer);
             }
-/*
-            if(pos->str(4)!="") power = stoi(pos->str(4));
-            if(pos->str(3)!="x") power=0;
-            if(pos->str(1)=="-") sign=0;
-            if(pos->str(2)!="") koef=sign?(stoi(pos->str(2))):(-stoi(pos->str(2)));
-            else if(pos->str(3)=="x") koef=sign?1:-1;
-            else koef=0;
-*//*
-            m[power]+=koef;
-            */
+            std::cout << answer << std::endl;
         }
-        else {
-            std::cout << "Nope :(" << std::endl;
-        }
-        for(auto& item : m)
-        {
-            std::cout << item.first << " : " << item.second << std::endl; //Вывод ключей и значений
-        }
-        /*
-        for(auto &kvp: m)
-        {
-           std::cout << kvp.first << std::endl;
-           for(auto &v: kvp.second) {
-              std::cout << v << std::endl;
-           }
-        }
-        */
-//    std::cout << "Hello World!" << std::endl;
-//    }
-    system("PAUSE"); // <---
+    }
     return 0;
-
 }
